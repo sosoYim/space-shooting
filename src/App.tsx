c3b0760fa09a6f3041ed48f5de4e89c2722ae208
sosoYim <sohyung.yim@gmail.com>
2023-10-02 21:29:34 +0900

    feat: 기본 조명, 컨트롤 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import { Canvas } from '@react-three/fiber'
 import Space from './components/Space'
+import { OrbitControls } from '@react-three/drei'
 
 function App() {
   return (
     <div className="w-screen h-screen">
       <Canvas dpr={[1, 2]}>
+        <ambientLight intensity={5} />
+        <axesHelper />
+        <OrbitControls />
         <Space />
       </Canvas>
     </div>
