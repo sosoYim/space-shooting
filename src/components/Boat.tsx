d8e2b57cc0153383f12083af63ba3f500a3b3216
sosoYim <sohyung.yim@gmail.com>
2023-10-02 22:45:02 +0900

    feat: 방향키로 배 이동

--- src/components/Boat.tsx
+++ src/components/Boat.tsx
@@ -2,7 +2,7 @@ import { useLoader } from '@react-three/fiber'
 import { MathUtils } from 'three'
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js'
 
-export default function Boat() {
+export default function Boat({ x = 0, z = 0 }: { x: number; z: number }) {
   const gltf = useLoader(GLTFLoader, '/models/boat.glb')
 
   return (
@@ -10,8 +10,8 @@ export default function Boat() {
       <pointLight position={[0, 1, 0]} power={20} distance={10} />
       <primitive
         object={gltf.scene}
-        position-x={0}
-        position-z={0}
+        position-x={x}
+        position-z={z}
         rotation={[0, MathUtils.degToRad(180), 0]}
       />
     </>
--- src/components/Space.tsx
+++ src/components/Space.tsx
@@ -1,5 +1,5 @@
 import { ThreeElements, useFrame, useThree } from '@react-three/fiber'
-import { useEffect, useRef } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import { useGame } from '../hooks/useGame'
 import Boat from './Boat'
 import dat from 'dat.gui'
@@ -17,6 +17,53 @@ function Box(props: ThreeElements['mesh']) {
 
 export default function Space() {
   const { start, startGame, endGame, duration } = useGame()
+  const [x, setX] = useState(0)
+  const [z, setZ] = useState(0)
+
+  const [move, setMove] = useState<
+    false | 'left' | 'right' | 'forward' | 'backward'
+  >(false)
+
+  useFrame((state, _) => {
+    if (!move) {
+      state.clock.stop()
+      return
+    }
+
+    if (move === 'left') {
+      setX((x) => x - 0.01)
+    } else if (move === 'right') {
+      setX((x) => x + 0.01)
+    } else if (move === 'forward') {
+      setZ((z) => z - 0.01)
+    } else if (move === 'backward') {
+      setZ((z) => z + 0.01)
+    }
+  })
+
+  addEventListener('keydown', (e) => {
+    if (e.key === 'ArrowLeft') {
+      setMove('left')
+    } else if (e.key === 'ArrowRight') {
+      setMove('right')
+    } else if (e.key === 'ArrowUp') {
+      setMove('forward')
+    } else if (e.key === 'ArrowDown') {
+      setMove('backward')
+    }
+  })
+
+  addEventListener('keyup', (e) => {
+    if (
+      e.key === 'ArrowLeft' ||
+      e.key === 'ArrowRight' ||
+      e.key === 'ArrowUp' ||
+      e.key === 'ArrowDown'
+    ) {
+      setMove(false)
+    }
+  })
+
   const camera = useThree(({ camera }) => camera)
 
   useEffect(() => {
@@ -30,10 +77,9 @@ export default function Space() {
 
   return (
     <>
-      <orthographicCamera position={[0, 0, 5]} zoom={3} />
       <Box position={[-1.2, 0, 0]} onClick={startGame} />
       <Box position={[1.2, 0, 0]} onClick={endGame} />
-      <Boat />
+      <Boat x={x} z={z} />
     </>
   )
 }
