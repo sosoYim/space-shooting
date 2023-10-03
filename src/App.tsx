e61072a3efa5988a2d5bf363924081099335e48c
sosoYim <sohyung.yim@gmail.com>
2023-10-03 13:12:40 +0900

    feat: add camera

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,20 @@ import { OrbitControls } from '@react-three/drei'
 function App() {
   return (
     <div className="w-screen h-screen">
-      <Canvas dpr={[1, 2]}>
+      <Canvas
+        dpr={[1, 2]}
+        orthographic
+        camera={{
+          // position: [0, 20, 20],
+          left: innerWidth / -2,
+          right: innerWidth / 2,
+          top: innerHeight / 2,
+          bottom: innerHeight / -2,
+          near: 1,
+          far: 1000,
+          zoom: 10,
+        }}
+      >
         <ambientLight intensity={5} />
         <axesHelper />
         <OrbitControls />
