638fc9461ad571efe8150de9115a9e84da2dc631
sosoYim <sohyung.yim@gmail.com>
2023-10-03 21:50:34 +0900

    feat: 총알에 맞으면 게임 종료

--- src/components/Space.tsx
+++ src/components/Space.tsx
@@ -1,4 +1,4 @@
-import { ThreeElements, useFrame, useThree } from '@react-three/fiber'
+import { useFrame, useThree } from '@react-three/fiber'
 import { useEffect, useRef } from 'react'
 import { useGame } from '../hooks/useGame'
 import Boat from './Boat'
@@ -7,31 +7,35 @@ import { useMove } from '../hooks/useMove'
 import Bombs from './Bombs'
 import StartButton from './StartButton'
 import ResultButton from './ResultButton'
-
-function Box(props: ThreeElements['mesh']) {
-  const meshRef = useRef<THREE.Mesh>(null!)
-  useFrame((_, delta) => (meshRef.current.rotation.x += delta))
-  return (
-    <mesh {...props} ref={meshRef} onClick={props.onClick}>
-      <boxGeometry args={[1, 1, 1]} />
-      <meshStandardMaterial color={'hotpink'} />
-    </mesh>
-  )
-}
+import { isIntersecting } from '../utils/isIntersecting'
 
 export default function Space() {
   const { start, startGame, endGame, duration } = useGame()
   const { x, z } = useMove()
 
   const camera = useThree(({ camera }) => camera)
-
   useEffect(() => {
     const gui = new dat.GUI()
     gui.add(camera.position, 'y', -100, 100, 0.01).name('카메라 Y')
     gui.add(camera.position, 'z', -100, 100, 0.01).name('카메라 Z')
   }, [])
 
-  console.log({ start, duration })
+  const bombsRef = useRef<THREE.Group>(null!)
+  const boatRef = useRef<THREE.Mesh>(null!)
+
+  useFrame(() => {
+    if (!bombsRef.current || !boatRef.current) return
+
+    if (
+      isIntersecting({
+        target: boatRef.current,
+        objects: bombsRef.current.children,
+      })
+    ) {
+      console.log('game over')
+      endGame()
+    }
+  })
 
   return (
     <>
@@ -40,9 +44,10 @@ export default function Space() {
       ) : (
         <StartButton start={start} startGame={startGame} duration={duration} />
       )}
-      <Box position={[1.2, 0, 0]} onClick={endGame} />
-      <Boat x={x} z={z} />
-      <Bombs fire={start} />
+      <mesh ref={boatRef}>
+        <Boat x={x} z={z} />
+      </mesh>
+      <Bombs fire={start} ref={bombsRef} />
     </>
   )
 }
